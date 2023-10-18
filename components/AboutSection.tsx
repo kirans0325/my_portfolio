af96f8e4e5d081dff564b0f0a22e37db0e985a63
kirans0325 <kirans0325@gmail.com>
2023-10-18 12:43:30 +0530

    frsh init

---
+++ components/AboutSection.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+// import Image from "next/image";
+
+const skills = [
+  { skill: "HTML" },
+  { skill: "CSS" },
+  { skill: "JavaScript" },
+  { skill: "TypeScript" },
+  { skill: "React" },
+  { skill: "Next.js" },
+  { skill: "Tailwind CSS" },
+  { skill: "Git" },
+  { skill: "GitHub" },
+  { skill: "Python" },
+  { skill: "Jupyter Notebooks" },
+];
+
+const AboutSection = () => {
+  return (
+    <section id="about">
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className="text-center font-bold text-4xl">
+          About Me
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
+
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          <div className="md:w-1/2 ">
+            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
+              Get to know me!
+            </h1>
+            <p>
+              Hi, my name is Kiran and I am a{" "}
+              <span className="font-bold">{"highly ambitious"}</span>,
+              <span className="font-bold">{" self-motivated"}</span>, and
+              <span className="font-bold">{" driven"}</span> software engineer
+              based in Vizag, India.
+            </p>
+            <br />
+            <p>
+              As a seasoned professional in the software industry, I graduated
+              from GMIT, Andhra Pradesh, in 2014 with a Bachelor's degree in
+              Chemical Engineering. Over the past nine years, I've cultivated
+              expertise in the pharmaceutical sector, but my true passion lies
+              in software engineering and web development. In the last 1.5
+              years, I've dedicated myself to mastering programming languages
+              and actively engaged in web development projects through
+              freelancing.
+            </p>
+            <br />
+            <p>
+              Now, I am actively seeking a permanent remote role that allows me
+              to leverage my chemical engineering background alongside my
+              newfound skills in the dynamic field of software development.
+            </p>
+            <br />
+            <p>
+              I believe that you should{" "}
+              <span className="font-bold text-teal-500">
+                never stop growing
+              </span>{" "}
+              and that&#39;s what I strive to do, I have a passion for
+              technology and a desire to always push the limits of what is
+              possible. I am excited to see where my career takes me and am
+              always open to new opportunities. 🙂
+            </p>
+          </div>
+          <div className="text-center md:w-1/2 md:text-left">
+            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                );
+              })}
+            </div>
+            {/* ß */}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
