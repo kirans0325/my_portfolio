a39268c9cffcb845e30918d061ffb3d92da93fbe
kirans0325 <kirans0325@gmail.com>
2023-10-26 10:22:34 +0530

    fixed navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -37,11 +37,10 @@ export default function Navbar() {
       <div className="justify-between md:items-center md:flex">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
-            <Link to="home">
-              <div className="container flex items-center space-x-2">
-                <h2 className="text-2xl font-bold">Kiran Seepana</h2>
-              </div>
-            </Link>
+            <div className="container flex items-center space-x-2">
+              <h2 className="text-2xl font-bold">Kiran Seepana</h2>
+            </div>
+
             <div className="md:hidden">
               <button
                 className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
